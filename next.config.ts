56fd9d7ec2c62a0326d364cb55aceeb9e1e54cdc
Mohammed Abu-Shqear <m.abushqear.dev@gmail.com>
2025-12-16 16:53:26 +0300

    Remove `basePath` and `assetPrefix` configurations from Next.js export settings.

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: '/my-porfolio',
-  assetPrefix: '/my-porfolio/',
   images: {
     unoptimized: true,
   },
