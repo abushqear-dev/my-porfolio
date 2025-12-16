4fb1df1e2189981f66213b1bae554970e1840441
Mohammed Abu-Shqear <m.abushqear.dev@gmail.com>
2025-12-16 16:11:26 +0300

    config: Add basePath and assetPrefix for /my-porfolio deployment.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: '/my-porfolio',
+  assetPrefix: '/my-porfolio/',
   images: {
     unoptimized: true,
   },
